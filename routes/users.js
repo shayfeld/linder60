5208a6bc1bd50887ce86611c58746ccfa54a00fe
shayfeld <86830613+shayfeld@users.noreply.github.com>
2022-02-22 00:32:15 +0200

    linder
    שינוי הכיתוב

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const request = require('request');
+const NodeGeocoder = require('node-geocoder');
+
+
 // User model
 const User = require('../models/User');
 
@@ -49,23 +52,6 @@ router.get('/register',(req, res)=>{
     res.render('register')
 });
 
-// Forgot Password Page
-router.get('/forgotpass',(req, res)=>{
-    res.render('forgotpass')
-});
-
-// Change Password Page
-router.get('/changepass/:id',(req, res)=>{
-    res.render('Changepass',{
-        id: req.params.id
-    });
-});
-
-// Sent email Page
-router.get('/sentEmail',(req, res)=>{
-    res.render('sentEmail')
-});
-
 // Register Handle
 router.post('/register',async(req, res, next)=>{
     const {userId, fullName, phone} = req.body;
@@ -98,6 +84,23 @@ router.post('/register',async(req, res, next)=>{
                     });
 
             }else{
+                // Find location with GPS
+                if(navigator.geolocation){
+                    const giveUp = 1000 * 30;
+                    const tooOld = 1000 * 60;
+                    options = {
+                        enableHighAccuracy: true,
+                        timeout: giveUp,
+                        maximumAge: tooOld
+                    }
+                    navigator.geolocation.getCurrentPosition(successfull(userId,
+                        password,
+                        fullName,
+                        phone),failer,this.options);
+                }else{
+                    console.log("old browser");
+                }
+
                 const newUser = new User({
                     userId,
                     password,
@@ -130,69 +133,33 @@ router.post('/register',async(req, res, next)=>{
 
 });
 
-// Send verification email
-const sendVerificationEmail = ({_id, email}, res) =>{
-    // url to be used in the mail
-    const currentUrl='https://garageservice.herokuapp.com/';
+function successfull(userId, password, fullName, phone){
+    const newUser = new User({
+        userId,
+        password,
+        fullName,
+        phone, 
+    });
 
-    const uniqueString = uuidv4() + _id;
-
-    // mail options
-    const mailOptions = {
-        from: process.env.AUTH_EMAIL,
-        to: email,
-        subject: 'Verify Your Email',
-        html: `<p> Verify your email address to complete the signup and login into your accout.</p><p>This link 
-        <b>expires in 6 hours</b>.</p><p>Press <a href=${
-            currentUrl + 'users/verify/' + _id + '/' + uniqueString
-        }>here</a> to proceed.</p>`
-    };
-
-    // hash the uniqueString
-    const saltRounds = 10;
-    bcrypt
-    .hash(uniqueString, saltRounds)
-    .then((hashedUniqueString) =>{
-        // set values in userVerification collection
-        const newVerification = new UserVerification({
-            userId: _id,
-            uniqueString: hashedUniqueString,
-            createLink: Date.now(),
-            expireLink: Date.now() + 21600000,
-        });
-        newVerification
-        .save()
-        .then(()=>{
-            transporter
-            .sendMail(mailOptions)
-            .then(()=>{
-                // email sent and verification record saved
-                res.redirect('/users/sentEmail');
-            })
-            .catch((err)=>{
-                console.log(err);
-                res.json({
-                    status: 'FAILED',
-                    message: 'Verification email failed',
-                });
-            })
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.json({
-                status: 'FAILED',
-                message: 'Could not save verification email data!',
-            });
-        })
+    // Hash Password
+    bcrypt.genSalt(10, (err, salt)=> 
+    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+    if(err) throw err;
+    // Set password to hashed
+    newUser.password = hash;
+    // Save user
+    newUser.save()
+    .then(user => {
+    // Account verification handler
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+        })(req, res, next);
     })
-    .catch((err) =>{
-        res.json({
-            status: 'FAILED',
-            message: 'An error occurred while hashing email data!',
-        });
-    })
-};
-
+    .catch(err => console.log(err));
+    }));
+}
 // verify email
 router.get('/verify/:userId/:uniqueString',(req,res)=>{
     const {userId, uniqueString} = req.params;
@@ -312,246 +279,11 @@ router.post('/login',async(req, res, next)=>{
     } 
 });
 
-// Forget Password Handle
-router.post('/forgotpass',(req, res)=>{
-    const {email} = req.body;
-    let errors = [];
-
-    //Check required field
-    if(!email){
-        errors.push({msg:'Please fill in Email field'});
-    }
-
-    if(errors.length > 0){
-        res.render('forgotpass',{
-            errors,
-            email
-        });
-    }else{
-        // Validation passed
-        User.findOne({email: email})
-        .then(user =>{
-            if(!user){
-                //User does not exists
-                errors.push({msg: 'Email is not found'});
-                res.render('forgotpass',{
-                    email: email
-                });
-            }else{
-                UserVerification.findOne({userId: user._id})
-                .then((verification)=>{
-                    if(verification){
-                        UserVerification.findByIdAndRemove(verification._id)
-                        .then(()=>{
-                            // Send mail for reset password
-                            sendChangePasswordEmail(user, res);
-                        })
-                        .catch((err)=>{
-                            console.log(err);
-                        });
-                    }else{
-                        // Send mail for reset password
-                        sendChangePasswordEmail(user, res);
-                    }
-                    
-                })
-                .catch((err)=>{
-                    console.log(err);
-                });
-                
-            }
-        });
-
-    }
-
-});
-
-// Send verification email
-const sendChangePasswordEmail = ({_id, email}, res) =>{
-    // url to be used in the mail
-    const currentUrl='https://garageservice.herokuapp.com/';
-
-    const uniqueString = uuidv4() + _id;
-
-    // mail options
-    const mailOptions = {
-        from: process.env.AUTH_EMAIL,
-        to: email,
-        subject: 'Reset Your Password',
-        html: `<p> Pleaes press the link to reset your password.</p><p>This link 
-        <b>expires in 5 minutes</b>.</p><p>Press <a href=${
-            currentUrl + 'users/resetPassword/' + _id + '/' + uniqueString
-        }>here</a> to proceed.</p>`
-    };
-
-    // hash the uniqueString
-    const saltRounds = 10;
-    bcrypt
-    .hash(uniqueString, saltRounds)
-    .then((hashedUniqueString) =>{
-        // set values in userVerification collection
-        const newVerification = new UserVerification({
-            userId: _id,
-            uniqueString: hashedUniqueString,
-            createLink: Date.now(),
-            expireLink: Date.now() + 300000,
-        });
-        newVerification
-        .save()
-        .then(()=>{
-            transporter
-            .sendMail(mailOptions)
-            .then(()=>{
-                // email sent and verification record saved
-                res.redirect('/users/sentEmail');
-            })
-            .catch((err)=>{
-                console.log(err);
-                res.json({
-                    status: 'FAILED',
-                    message: 'Verification email failed',
-                });
-            })
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.json({
-                status: 'FAILED',
-                message: 'Could not save verification email data!',
-            });
-        })
-    })
-    .catch((err) =>{
-        res.json({
-            status: 'FAILED',
-            message: 'An error occurred while hashing email data!',
-        });
-    })
-};
-
-// verify email
-router.get('/resetPassword/:userId/:uniqueString',(req,res)=>{
-    const {userId, uniqueString} = req.params;
-
-    UserVerification
-    .find({userId})
-    .then((result)=>{
-        if(result.length >0){
-            // User verification record exists
-            const {expireLink} = result[0];
-            const hashedUniqueString = result[0].uniqueString;
-            // Check for expired unique string
-            if(expireLink < Date.now()){
-                // record has expired
-                UserVerification.deleteOne({userId})
-                .then((result)=>{
-                    User
-                    .deleteOne({_id:userId})
-                    .then(()=>{
-                        const message ='Link has expired. Please sign up again.';
-                        res.redirect(`/users/verified/error=true&message=${message}`);
-                    })
-                    .catch((err)=>{
-                        console.log(err);
-                        const message ='Clearing user with expired unique string failed.';
-                        res.redirect(`/users/verified/error=true&message=${message}`);
-                    })
-                })
-                .catch((err)=>{
-                    console.log(err);
-                    const message ='An error occurred while clearing expired user verification record.';
-                    res.redirect(`/users/verified/error=true&message=${message}`);
-                })
-            }else{
-                // Valid record exists
-
-                // Compare the hashed unique string
-                bcrypt
-                .compare(uniqueString, hashedUniqueString)
-                .then((result)=>{
-                    if(result){
-                        res.redirect(`/users/changepass/${userId}`);
-                    }else{
-                       // Existing record but incorrect verification details passed 
-                        const message ='Invalid verification details passed. Check your inbox.';
-                        res.redirect(`/users/verified/error=true&message=${message}`);
-                    }
-                })
-                .catch((err)=>{
-                    const message ='An error occurred while comparing unique strings.';
-                    res.redirect(`/users/verified/error=true&message=${message}`);
-                })
-            }
-        }else{
-            // User verification record does'nt exists
-            const message ='Account record does not exist or has verified already. Please sign up or log in.';
-            res.redirect(`/users/verified/error=true&message=${message}`);
-        }
-    })
-    .catch((err)=>{
-        console.log(err);
-        const message ='An error occurred while checking for existing user verification record.';
-        res.redirect(`/users/verified/error=true&message=${message}`);
-    })
-});
-
-// Change Password Handle // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-router.post('/changepass/:id',(req, res)=>{
-    const {password, password2} = req.body;
-    const userId = req.params.id;
-    console.log(userId);
-
-    let errors = [];
-
-    //Check required fields
-    if(!password || !password2){
-        errors.push({msg:'Please fill in all fields'});
-    }
-
-    //Check passwords match
-    if(password !== password2){
-        errors.push({msg:'Passwords do nt match'});
-    }
-
-    //Check passwords length
-    if(password.length < 6){
-        errors.push({msg:'Passwords Should be at least 6 characters'});
-    }
-    
-    if(errors.length > 0){
-        res.render('changepass',{
-                    errors,
-                    email,
-                    password,
-                });
-    }
-    else{
-        // Hash Password
-        bcrypt.genSalt(10, (err, salt)=> 
-            bcrypt.hash(password, salt, (err, hash)=>{
-                if(err) throw err;
-                // Set password to hashed
-                User
-                .updateOne({_id:userId}, {password: hash})
-                .then(()=>{
-                    UserVerification.deleteOne({userId})
-                    .then(()=>{
-                        res.redirect('/users/login');
-                    })
-                    .catch((err)=>{
-                        console.log(err);
-                        const message ='An error occurred while finalizing successful changing password.';
-                        res.redirect(`/users/verified/error=true&message=${message}`);
-                    })
-                })
-                .catch((err)=>{
-                    console.log(err);
-                    const message ='An error occurred while updateing user record to show verified.';
-                    res.redirect(`/users/verified/error=true&message=${message}`);
-                });
-
-        }));
-    }
+// Logout Handle
+router.post('/logout',(req, res, next)=>{
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
 });
 
 
